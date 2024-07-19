55c553476daf0a8613f887271f0e5812790e760d
Song Jimin <songwlals050328@gmail.com>
2024-07-20 03:16:49 +0900

    회원가입 DTO JoinRequestDTO 생성

---
+++ src/main/java/hackerton/wakeup/member/entity/dto/request/JoinRequestDTO.java
@@ -0,0 +1,9 @@
+package hackerton.wakeup.member.entity.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class JoinRequestDTO {
+}
--- src/main/java/hackerton/wakeup/member/service/MemberService.java
+++ src/main/java/hackerton/wakeup/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package hackerton.wakeup.member.service;
 
+import hackerton.wakeup.member.entity.Member;
+
 public interface MemberService {
     boolean checkEmailDuplication(String email);
 }
