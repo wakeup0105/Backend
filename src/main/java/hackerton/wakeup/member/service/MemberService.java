7efc8961c180064590b06072eb74ed5e75754da3
Song Jimin <songwlals050328@gmail.com>
2024-07-20 03:22:41 +0900

    MemberService에 회원가입용 joinMember() method 추가

--- src/main/java/hackerton/wakeup/member/service/MemberService.java
+++ src/main/java/hackerton/wakeup/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package hackerton.wakeup.member.service;
 
 import hackerton.wakeup.member.entity.Member;
+import hackerton.wakeup.member.entity.dto.request.JoinRequestDTO;
 
 public interface MemberService {
     boolean checkEmailDuplication(String email);
+    void joinMember(JoinRequestDTO req);
 }
