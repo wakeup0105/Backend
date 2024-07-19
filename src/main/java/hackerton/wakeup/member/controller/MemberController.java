91eb87baa4dcef918e6ed9218a1218df32ee925d
Song Jimin <songwlals050328@gmail.com>
2024-07-20 03:33:13 +0900

    Member 도메인의 Controller인 MemberController 생성

---
+++ src/main/java/hackerton/wakeup/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package hackerton.wakeup.member.controller;
+
+import hackerton.wakeup.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+    private final MemberService memberService;
+}
