a1dc149f844da12d60cd62158338edcef679faf8
Song Jimin <songwlals050328@gmail.com>
2024-07-20 03:17:41 +0900

    email 필드 추가

--- src/main/java/hackerton/wakeup/member/entity/dto/request/JoinRequestDTO.java
+++ src/main/java/hackerton/wakeup/member/entity/dto/request/JoinRequestDTO.java
@@ -1,9 +1,12 @@
 package hackerton.wakeup.member.entity.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class JoinRequestDTO {
+    @NotBlank(message = "이메일이 비어있습니다.")
+    private String email;
 }
