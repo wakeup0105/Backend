717a42cdee2ffb69068093e3d1cbf0d841fb957d
Song Jimin <songwlals050328@gmail.com>
2024-07-21 07:25:10 +0900

    생성과 검증을 테스트하는 verificationLogicTest() method 구현

--- src/test/java/hackerton/wakeup/email/service/EmailVerifyServiceImplTest.java
+++ src/test/java/hackerton/wakeup/email/service/EmailVerifyServiceImplTest.java
@@ -1,20 +1,29 @@
 package hackerton.wakeup.email.service;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailVerifyServiceImplTest {
 
-    @Test
-    void generateVerificationCode() {
+    EmailVerifyService emailVerifyService;
+    @BeforeEach
+    public void setUp() {
+        emailVerifyService = new EmailVerifyServiceImpl();
     }
 
     @Test
-    void verifyCode() {
-    }
-
-    @Test
-    void randomCode() {
+    @DisplayName("해당 이메일로 발급 받은 코드와 일치하다면 true")
+    void verificationLogicTest() {
+        //given
+        String email = "test@test.com";
+        //when
+        String verificationCode = emailVerifyService.generateVerificationCode(email);
+        //then
+        assertThat(emailVerifyService.verifyCode(email, verificationCode)).isEqualTo(true);
     }
 }
