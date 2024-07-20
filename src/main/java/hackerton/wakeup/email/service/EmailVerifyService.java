1ae447f15aa3290a5a904833a5944195f5c6322d
Song Jimin <songwlals050328@gmail.com>
2024-07-21 06:47:53 +0900

    코드 검증용 verifyCode() method 추가

--- src/main/java/hackerton/wakeup/email/service/EmailVerifyService.java
+++ src/main/java/hackerton/wakeup/email/service/EmailVerifyService.java
@@ -2,4 +2,5 @@ package hackerton.wakeup.email.service;
 
 public interface EmailVerifyService {
     public String generateVerificationCode(String email);
+    public boolean verifyCode(String email, String code);
 }
