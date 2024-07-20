2fe67ee7cf0338fe063c5511c4a1180b0077a537
Song Jimin <songwlals050328@gmail.com>
2024-07-21 06:51:51 +0900

    랜덤 코드 생성용 randomCode() method 생성

--- src/main/java/hackerton/wakeup/email/service/EmailVerifyService.java
+++ src/main/java/hackerton/wakeup/email/service/EmailVerifyService.java
@@ -3,4 +3,5 @@ package hackerton.wakeup.email.service;
 public interface EmailVerifyService {
     public String generateVerificationCode(String email);
     public boolean verifyCode(String email, String code);
+    public String randomCode();
 }
