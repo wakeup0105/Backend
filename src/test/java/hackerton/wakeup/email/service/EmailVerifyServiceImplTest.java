a3b56b135de00677cde50711b351b20fe1447e8e
Song Jimin <songwlals050328@gmail.com>
2024-07-21 07:16:56 +0900

    EmailVerifyServiceImplTest 생성

---
+++ src/test/java/hackerton/wakeup/email/service/EmailVerifyServiceImplTest.java
@@ -0,0 +1,20 @@
+package hackerton.wakeup.email.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailVerifyServiceImplTest {
+
+    @Test
+    void generateVerificationCode() {
+    }
+
+    @Test
+    void verifyCode() {
+    }
+
+    @Test
+    void randomCode() {
+    }
+}
