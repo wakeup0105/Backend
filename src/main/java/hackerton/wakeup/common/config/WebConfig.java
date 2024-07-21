c4f58bdd4ac66b6da25b308391b3c271bf85c91b
Song Jimin <songwlals050328@gmail.com>
2024-07-22 04:58:15 +0900

    addCorsMappings method 생성

--- src/main/java/hackerton/wakeup/common/config/WebConfig.java
+++ src/main/java/hackerton/wakeup/common/config/WebConfig.java
@@ -1,8 +1,12 @@
 package hackerton.wakeup.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {}
 }
