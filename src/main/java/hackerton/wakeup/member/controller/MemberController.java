61751abc94ab2e37d26ef2e524d286a72ab3a3e5
Song Jimin <songwlals050328@gmail.com>
2024-07-22 02:22:25 +0900

    MemberController에 /api/member/send-verification 경로 POST요청인 인증코드 전송용 sendVerification() method 추가

--- src/main/java/hackerton/wakeup/member/controller/MemberController.java
+++ src/main/java/hackerton/wakeup/member/controller/MemberController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +24,14 @@ public class MemberController {
         }
         memberService.joinMember(req);
         return ResponseEntity.ok("회원가입 성공");
+    }
+
+    @PostMapping("/send-verification")
+    public ResponseEntity<String> sendVerification(@RequestParam String email){
+        if (memberService.checkEmailDuplication(email)){
+            return new ResponseEntity<>("이미 존재하는 이메일입니다.", HttpStatus.BAD_REQUEST);
+        }
+        memberService.sendVerificationEmail(email);
+        return ResponseEntity.ok("인증코드가 이메일로 전송 되었습니다.");
     }
 }
