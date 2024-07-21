f74e22a32376d288cbd7ce98b311726a400bfaec
Song Jimin <songwlals050328@gmail.com>
2024-07-22 04:57:20 +0900

    CORS 전역 설정을 위한 WebConfig class 생성

---
+++ src/main/java/hackerton/wakeup/common/config/WebConfig.java
@@ -0,0 +1,8 @@
+package hackerton.wakeup.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+}
