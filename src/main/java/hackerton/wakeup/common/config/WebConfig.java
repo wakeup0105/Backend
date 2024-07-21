416e2ce19402ce0333f4bf719e9e819ec12a73d5
Song Jimin <songwlals050328@gmail.com>
2024-07-22 05:18:47 +0900

    exposedHeaders: 노출할 헤더 전체로 수정

--- src/main/java/hackerton/wakeup/common/config/WebConfig.java
+++ src/main/java/hackerton/wakeup/common/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 적용할 URL 패턴
-                .allowedOrigins("*") // 허용된 Origin
+                .allowedOrigins("*") // 허용된 Origin: 프론트엔드 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용된 Http Methods
                 .allowedHeaders("Authorization", "Content-Type") // 허용된 헤더
-                .exposedHeaders("Custom-Header") // 노출할 헤더값
+                .exposedHeaders("*") // 노출할 헤더값
                 .allowCredentials(false) // 쿠키 인증 요청
                 .maxAge(3600); // pre-flight request 캐싱 시간
     }
