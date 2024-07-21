1cbe9377d76927a4bcbde1039314d5f2b108a46f
Song Jimin <songwlals050328@gmail.com>
2024-07-22 05:06:50 +0900

    cors 기본 설정 구현

--- src/main/java/hackerton/wakeup/common/config/WebConfig.java
+++ src/main/java/hackerton/wakeup/common/config/WebConfig.java
@@ -8,5 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {}
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 적용할 URL 패턴
+                .allowedOrigins("*") // 허용된 Origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용된 Http Methods
+                .allowedHeaders("Authorization", "Content-Type") // 허용된 헤더
+                .exposedHeaders("Custom-Header") // 노출할 헤더값
+                .allowCredentials(false) // 쿠키 인증 요청
+                .maxAge(3600); // pre-flight request 캐싱 시간
+    }
 }
