b5ec10c4f4d53d5e670b95bf92e4bf779ca1ba90
Song Jimin <songwlals050328@gmail.com>
2024-07-22 16:44:04 +0900

    JwtTokenFilter 생성

---
+++ src/main/java/hackerton/wakeup/common/security/JwtTokenFilter.java
@@ -0,0 +1,20 @@
+package hackerton.wakeup.common.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
