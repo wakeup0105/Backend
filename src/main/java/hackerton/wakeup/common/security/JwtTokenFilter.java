a26d3ee92ba175bbc6c4ad7e4c9b368ef2414cc4
Song Jimin <songwlals050328@gmail.com>
2024-07-22 17:12:55 +0900

    JwtTokenFilter에 만료시간 인증과 로그인정보 저장 로직 추가

--- src/main/java/hackerton/wakeup/common/security/JwtTokenFilter.java
+++ src/main/java/hackerton/wakeup/common/security/JwtTokenFilter.java
@@ -1,5 +1,6 @@
 package hackerton.wakeup.common.security;
 
+import hackerton.wakeup.member.entity.Member;
 import hackerton.wakeup.member.service.MemberService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,9 +8,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
@@ -24,12 +29,29 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         //인증헤더 받아옴
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-        //헤더가 비어있거나 Bearer로 시작하지 않는다면 -> 검증 불가
+        //헤더가 비어있거나 Bearer로 시작하지 않는다면 -> 인증 X
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
         String token = authHeader.split(" ")[1];
+
+        //만료시간이 지났다면 -> 인증 X
+        if (JwtTokenUtil.isExpired(token, secretKey)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String email = JwtTokenUtil.getEmail(token, secretKey);
+
+        Member memberByEmail = memberService.getMemberByEmail(email).orElse(null);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                memberByEmail.getEmail(),null, null);
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
     }
 }
