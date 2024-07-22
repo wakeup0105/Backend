86f4d0654a7f59bd364f90fb817aecfa0d255d79
Song Jimin <songwlals050328@gmail.com>
2024-07-22 16:33:34 +0900

    SecurityConfig에 memberService, secretKey 필드 추가

--- src/main/java/hackerton/wakeup/common/security/SecurityConfig.java
+++ src/main/java/hackerton/wakeup/common/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package hackerton.wakeup.common.security;
 
+import hackerton.wakeup.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -8,4 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final MemberService memberService;
+    @Value("${spring.jwt.secretKey}")
+    private String secretKey;
 }
