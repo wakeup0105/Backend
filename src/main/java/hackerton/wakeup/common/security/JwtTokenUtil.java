35d5676bbd07dc48ec4627461a1c7ed9c2643e15
Song Jimin <songwlals050328@gmail.com>
2024-07-22 16:54:32 +0900

    JwtTokenUtil 생성 & 토큰 생성용 createToken() method 작성

---
+++ src/main/java/hackerton/wakeup/common/security/JwtTokenUtil.java
@@ -0,0 +1,22 @@
+package hackerton.wakeup.common.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtTokenUtil {
+
+    public static String createToken(String email, String key, long expiresInSeconds) {
+        Claims claims = Jwts.claims();
+        claims.put("email", email);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiresInSeconds))
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+}
