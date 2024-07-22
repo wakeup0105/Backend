bdf752e742776b12e7c29ff7c52a10b0c9a1fae0
Song Jimin <songwlals050328@gmail.com>
2024-07-22 16:45:21 +0900

    JwtTokenFilter에 memberService, secretKey 필드 추가

--- src/main/java/hackerton/wakeup/common/security/JwtTokenFilter.java
+++ src/main/java/hackerton/wakeup/common/security/JwtTokenFilter.java
@@ -1,5 +1,6 @@
 package hackerton.wakeup.common.security;
 
+import hackerton.wakeup.member.service.MemberService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,9 +13,12 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
 
+    private final MemberService memberService;
+    private final String secretKey;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
     }
 }
