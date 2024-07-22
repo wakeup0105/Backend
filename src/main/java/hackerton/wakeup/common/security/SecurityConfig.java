47b6b3e3fb9cba02d60f70a67cac3762229e10cd
Song Jimin <songwlals050328@gmail.com>
2024-07-22 16:34:19 +0900

    비밀번호 암호화용 passwordEncoder 추가

--- src/main/java/hackerton/wakeup/common/security/SecurityConfig.java
+++ src/main/java/hackerton/wakeup/common/security/SecurityConfig.java
@@ -3,8 +3,11 @@ package hackerton.wakeup.common.security;
 import hackerton.wakeup.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,4 +16,9 @@ public class SecurityConfig {
     private final MemberService memberService;
     @Value("${spring.jwt.secretKey}")
     private String secretKey;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
