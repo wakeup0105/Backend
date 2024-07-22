567873e519a87ea13d6f4083cb15118561391f81
Song Jimin <songwlals050328@gmail.com>
2024-07-22 16:50:42 +0900

    JwtTokenFilter에 인증헤더를 받아오는 로직 추가

--- src/main/java/hackerton/wakeup/common/security/JwtTokenFilter.java
+++ src/main/java/hackerton/wakeup/common/security/JwtTokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -20,5 +21,15 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        //인증헤더 받아옴
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        //헤더가 비어있거나 Bearer로 시작하지 않는다면 -> 검증 불가
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.split(" ")[1];
     }
 }
