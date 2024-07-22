dc5b9d5e0047d77637108e67c690be372dfc4126
Song Jimin <songwlals050328@gmail.com>
2024-07-22 15:13:58 +0900

    SecurityConfig 생성

---
+++ src/main/java/hackerton/wakeup/common/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package hackerton.wakeup.common.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
