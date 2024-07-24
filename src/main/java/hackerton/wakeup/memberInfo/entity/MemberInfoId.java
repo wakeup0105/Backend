ade5e435203dd34f56f60c8be1b778ed37fb3d69
Song Jimin <songwlals050328@gmail.com>
2024-07-24 21:31:10 +0900

    MemberInfoId 클래스에 @Data 어노테이션 추가

--- src/main/java/hackerton/wakeup/memberInfo/entity/MemberInfoId.java
+++ src/main/java/hackerton/wakeup/memberInfo/entity/MemberInfoId.java
@@ -1,7 +1,10 @@
 package hackerton.wakeup.memberInfo.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class MemberInfoId implements Serializable {
     private Long id;
     private Long member;
