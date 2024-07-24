cc6a0486a60018934741892e009d8654b91be3da
Song Jimin <songwlals050328@gmail.com>
2024-07-25 05:08:14 +0900

    password 필드 추가

--- src/main/java/hackerton/wakeup/member/entity/dto/request/ChangePasswordRequestDTO.java
+++ src/main/java/hackerton/wakeup/member/entity/dto/request/ChangePasswordRequestDTO.java
@@ -1,5 +1,6 @@
 package hackerton.wakeup.member.entity.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,4 +9,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ChangePasswordRequestDTO {
+    @NotBlank(message = "비밀번호가 비어있습니다.")
+    private String password;
 }
