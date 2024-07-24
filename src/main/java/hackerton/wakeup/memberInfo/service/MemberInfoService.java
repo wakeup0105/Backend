04a1b5095e483764c0bb05d3aaa00c354bfa3b18
Song Jimin <songwlals050328@gmail.com>
2024-07-25 07:21:03 +0900

    복합키로 객체를 불러오는 findById() method 생성

--- src/main/java/hackerton/wakeup/memberInfo/service/MemberInfoService.java
+++ src/main/java/hackerton/wakeup/memberInfo/service/MemberInfoService.java
@@ -1,9 +1,11 @@
 package hackerton.wakeup.memberInfo.service;
 
 import hackerton.wakeup.memberInfo.entity.MemberInfo;
+import hackerton.wakeup.memberInfo.entity.MemberInfoId;
 
 public interface MemberInfoService {
     boolean checkNicknameAndTagDuplication(String nickname, String tag);
     String settingNickname(String nickname, String tag);
+    MemberInfo findById(MemberInfoId id);
     MemberInfo findByMemberId(Long member_id);
 }
