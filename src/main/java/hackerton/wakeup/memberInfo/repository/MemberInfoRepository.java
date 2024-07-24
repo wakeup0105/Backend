eb6de0493d92c6a05b4792dcdc9844312e37751d
Song Jimin <songwlals050328@gmail.com>
2024-07-25 07:08:59 +0900

    복합키로 객체를 불러오는 findById() method 생성

--- src/main/java/hackerton/wakeup/memberInfo/repository/MemberInfoRepository.java
+++ src/main/java/hackerton/wakeup/memberInfo/repository/MemberInfoRepository.java
@@ -5,7 +5,10 @@ import hackerton.wakeup.memberInfo.entity.MemberInfoId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberInfoRepository extends JpaRepository<MemberInfo, MemberInfoId> {
     boolean existsByNicknameAndTag(String nickname, String tag);
+
+    Optional<MemberInfo> findById(MemberInfoId id);
 }
