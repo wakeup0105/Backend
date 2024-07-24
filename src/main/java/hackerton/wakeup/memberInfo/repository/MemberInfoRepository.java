a7a94203b7533ec468966987342e0b010c0a4c85
Song Jimin <songwlals050328@gmail.com>
2024-07-25 06:37:42 +0900

    MemberInfoRepository 생성

---
+++ src/main/java/hackerton/wakeup/memberInfo/repository/MemberInfoRepository.java
@@ -0,0 +1,8 @@
+package hackerton.wakeup.memberInfo.repository;
+
+import hackerton.wakeup.memberInfo.entity.MemberInfo;
+import hackerton.wakeup.memberInfo.entity.MemberInfoId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberInfoRepository extends JpaRepository<MemberInfo, MemberInfoId> {
+}
