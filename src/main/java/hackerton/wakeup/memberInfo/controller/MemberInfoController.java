a3af6d749e91ce23a0dc8e7825927bd654cd644a
Song Jimin <songwlals050328@gmail.com>
2024-07-25 08:01:20 +0900

    /api/member-info/set-nickname 경로에 POST로 닉네임 초기 설정하는 setNickname() method 생성

--- src/main/java/hackerton/wakeup/memberInfo/controller/MemberInfoController.java
+++ src/main/java/hackerton/wakeup/memberInfo/controller/MemberInfoController.java
@@ -1,13 +1,21 @@
 package hackerton.wakeup.memberInfo.controller;
 
+import hackerton.wakeup.memberInfo.entity.dto.request.SetNicknameRequestDTO;
 import hackerton.wakeup.memberInfo.service.MemberInfoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/member-info")
 public class MemberInfoController {
     private final MemberInfoService memberInfoService;
+
+    @PostMapping("/set-nickname")
+    public ResponseEntity<String> setNickname(@Valid @RequestBody SetNicknameRequestDTO req, Authentication auth){
+
+    }
 }
