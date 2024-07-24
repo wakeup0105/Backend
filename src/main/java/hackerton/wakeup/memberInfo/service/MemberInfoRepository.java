d4c08151bb9b196f7468939cbbfd2243fa55fee4
Song Jimin <songwlals050328@gmail.com>
2024-07-25 07:17:02 +0900

    닉네임과 태그를 생성하여 저장하는 settingNickname() method 생성

--- src/main/java/hackerton/wakeup/memberInfo/service/MemberInfoRepository.java
+++ src/main/java/hackerton/wakeup/memberInfo/service/MemberInfoRepository.java
@@ -2,4 +2,5 @@ package hackerton.wakeup.memberInfo.service;
 
 public interface MemberInfoRepository {
     boolean checkNicknameAndTagDuplication(String nickname, String tag);
+    String settingNickname(String nickname, String tag);
 }
