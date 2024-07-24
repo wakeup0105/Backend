11610c7dca976453ce4f453058d50000564d55df
Song Jimin <songwlals050328@gmail.com>
2024-07-25 06:34:33 +0900

    닉네임 설정용 SetNicknameRequestDTO 생성

---
+++ src/main/java/hackerton/wakeup/memberInfo/entity/dto/SetNicknameRequestDTO.java
@@ -0,0 +1,11 @@
+package hackerton.wakeup.memberInfo.entity.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SetNicknameRequestDTO {
+}
