4cf89ffd73969dd9bb6eb077824c42997127dd0f
Song Jimin <songwlals050328@gmail.com>
2024-07-25 05:06:49 +0900

    비밀번호 변경용 ChangePasswordRequestDTO 생성

---
+++ src/main/java/hackerton/wakeup/member/entity/dto/request/ChangePasswordRequestDTO.java
@@ -0,0 +1,11 @@
+package hackerton.wakeup.member.entity.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChangePasswordRequestDTO {
+}
