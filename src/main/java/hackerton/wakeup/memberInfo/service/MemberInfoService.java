9c2ed7aeec6999aa3626e5c16c23918afdb29dfe
Song Jimin <songwlals050328@gmail.com>
2024-07-25 07:21:50 +0900

    앞선 두 method 반환 타입 변경: MemberInfo -> Optional<MemberInfo>

--- src/main/java/hackerton/wakeup/memberInfo/service/MemberInfoService.java
+++ src/main/java/hackerton/wakeup/memberInfo/service/MemberInfoService.java
@@ -3,9 +3,11 @@ package hackerton.wakeup.memberInfo.service;
 import hackerton.wakeup.memberInfo.entity.MemberInfo;
 import hackerton.wakeup.memberInfo.entity.MemberInfoId;
 
+import java.util.Optional;
+
 public interface MemberInfoService {
     boolean checkNicknameAndTagDuplication(String nickname, String tag);
     String settingNickname(String nickname, String tag);
-    MemberInfo findById(MemberInfoId id);
-    MemberInfo findByMemberId(Long member_id);
+    Optional<MemberInfo> findById(MemberInfoId id);
+    Optional<MemberInfo> findByMemberId(Long member_id);
 }
