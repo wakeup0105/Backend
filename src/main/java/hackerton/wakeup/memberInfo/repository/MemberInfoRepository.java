5fbe9edba66e699c2e63ab810a1743aacf4e5d8b
Song Jimin <songwlals050328@gmail.com>
2024-07-25 07:05:08 +0900

    닉네임 확인용 existsByNickname() method 삭제

--- src/main/java/hackerton/wakeup/memberInfo/repository/MemberInfoRepository.java
+++ src/main/java/hackerton/wakeup/memberInfo/repository/MemberInfoRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MemberInfoRepository extends JpaRepository<MemberInfo, MemberInfoId> {
-    boolean existsByNickname(String nickname);
     boolean existsByNicknameAndTag(String nickname, String tag);
 }
