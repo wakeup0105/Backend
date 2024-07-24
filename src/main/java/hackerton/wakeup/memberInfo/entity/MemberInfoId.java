beebdfb96aa41fe7b49c02dad427b7c675a06db0
Song Jimin <songwlals050328@gmail.com>
2024-07-24 21:43:57 +0900

    MemberInfoId 클래스에 생성자 어노테이션과 Embeddable 어노테이션 추가

--- src/main/java/hackerton/wakeup/memberInfo/entity/MemberInfoId.java
+++ src/main/java/hackerton/wakeup/memberInfo/entity/MemberInfoId.java
@@ -1,10 +1,16 @@
 package hackerton.wakeup.memberInfo.entity;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
 public class MemberInfoId implements Serializable {
     private Long id;
     private Long member;
