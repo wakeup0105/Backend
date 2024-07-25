537a3234061213682c6d78410f7669329f1f5d00
Song Jimin <songwlals050328@gmail.com>
2024-07-25 09:03:18 +0900

    MemberInfo Entity nickname 필드 annotation 변경: @Min, @Max -> @Size

--- src/main/java/hackerton/wakeup/memberInfo/entity/MemberInfo.java
+++ src/main/java/hackerton/wakeup/memberInfo/entity/MemberInfo.java
@@ -2,13 +2,8 @@ package hackerton.wakeup.memberInfo.entity;
 
 import hackerton.wakeup.member.entity.Member;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 @Entity
 @Table(name = "MemberInfo")
@@ -27,8 +22,7 @@ public class MemberInfo {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @Max(30)
-    @Min(2)
+    @Size(min = 2, max = 30)
     @NotBlank
     private String nickname;
 
