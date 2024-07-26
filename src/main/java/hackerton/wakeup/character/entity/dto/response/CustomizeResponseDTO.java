c0e4651283ba8d1ef41856364bfeceb243d72bc3
Song Jimin <songwlals050328@gmail.com>
2024-07-27 04:36:35 +0900

    CustomizeResponseDTO에 요청에 맞는 변환용 toEntity() method 구현

--- src/main/java/hackerton/wakeup/character/entity/dto/response/CustomizeResponseDTO.java
+++ src/main/java/hackerton/wakeup/character/entity/dto/response/CustomizeResponseDTO.java
@@ -1,5 +1,6 @@
 package hackerton.wakeup.character.entity.dto.response;
 
+import hackerton.wakeup.character.entity.Character;
 import lombok.*;
 
 @Getter
@@ -10,4 +11,13 @@ public class CustomizeResponseDTO {
     private String mouth;
     private String head;
     private String body;
+
+    public CustomizeResponseDTO toEntity(Character character){
+        CustomizeResponseDTO customizeResponseDTO = new CustomizeResponseDTO();
+        customizeResponseDTO.setEyes(character.getEyes());
+        customizeResponseDTO.setMouth(character.getMouth());
+        customizeResponseDTO.setHead(character.getHead());
+        customizeResponseDTO.setBody(character.getBody());
+        return customizeResponseDTO;
+    }
 }
