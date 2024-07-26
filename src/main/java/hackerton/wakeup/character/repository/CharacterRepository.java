97d1e5b3bcfa5b0879018fcef90c2391bc47da3d
Song Jimin <songwlals050328@gmail.com>
2024-07-27 01:03:19 +0900

    멤버 id로 데이터를 찾는 findByIdMember() method 추가

--- src/main/java/hackerton/wakeup/character/repository/CharacterRepository.java
+++ src/main/java/hackerton/wakeup/character/repository/CharacterRepository.java
@@ -3,5 +3,8 @@ package hackerton.wakeup.character.repository;
 import hackerton.wakeup.character.entity.CharacterId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CharacterRepository extends JpaRepository<Character, CharacterId> {
+    Optional<Character> findByIdMember(Long member);
 }
