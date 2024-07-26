9431b4240aa80872f99e961d007156011d0af95b
Song Jimin <songwlals050328@gmail.com>
2024-07-27 01:08:08 +0900

    초기에 데이터를 저장하는 initCharacter() method 추가

--- src/main/java/hackerton/wakeup/character/service/CharacterService.java
+++ src/main/java/hackerton/wakeup/character/service/CharacterService.java
@@ -1,7 +1,9 @@
 package hackerton.wakeup.character.service;
 
 import hackerton.wakeup.character.entity.Character;
+import hackerton.wakeup.member.entity.Member;
 
 public interface CharacterService {
+    Character initCharacter(Member member);
     Character getByMemberId(Long member_id);
 }
