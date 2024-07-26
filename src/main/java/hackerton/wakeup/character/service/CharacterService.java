f819bba64afe0188270bf59bbe4eb636c5e5d098
Song Jimin <songwlals050328@gmail.com>
2024-07-27 01:05:35 +0900

    멤버 id로 데이터를 찾는 getByMemberId() method 추가

--- src/main/java/hackerton/wakeup/character/service/CharacterService.java
+++ src/main/java/hackerton/wakeup/character/service/CharacterService.java
@@ -1,4 +1,7 @@
 package hackerton.wakeup.character.service;
 
+import hackerton.wakeup.character.entity.Character;
+
 public interface CharacterService {
+    Character getByMemberId(Long member_id);
 }
