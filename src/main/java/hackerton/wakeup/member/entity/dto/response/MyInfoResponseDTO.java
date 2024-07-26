5f7fd915737649c15c63dd1f310329f94491418d
Song Jimin <songwlals050328@gmail.com>
2024-07-26 22:54:38 +0900

    나의 정보 관리 페이지 응답용 DTO 생성

---
+++ src/main/java/hackerton/wakeup/member/entity/dto/response/MyInfoResponseDTO.java
@@ -0,0 +1,11 @@
+package hackerton.wakeup.member.entity.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MyInfoResponseDTO {
+}
