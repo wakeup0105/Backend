70004cc59faad2ffaae9a41d85b3de0289ddd09b
Song Jimin <songwlals050328@gmail.com>
2024-07-26 23:21:35 +0900

    MemberController /find-account path에 유저찾기 method() 인증부분 임시 주석

--- src/main/java/hackerton/wakeup/member/controller/MemberController.java
+++ src/main/java/hackerton/wakeup/member/controller/MemberController.java
@@ -57,9 +57,9 @@ public class MemberController {
         if (!memberService.checkEmailDuplication(req.getEmail())){
             return new ResponseEntity<>("이메일이 존재하지 않습니다.", HttpStatus.BAD_REQUEST);
         }
-        if(!emailVerifyService.verifyCode(req.getEmail(), req.getVerificationCode())){
-            return new ResponseEntity<>("인증코드가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
-        }
+//        if(!emailVerifyService.verifyCode(req.getEmail(), req.getVerificationCode())){
+//            return new ResponseEntity<>("인증코드가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+//        }
         String token = JwtTokenUtil.createToken(req.getEmail(), secretKey, Long.parseLong(expirationTime));
         return ResponseEntity.ok("인증성공, Token: " + token);
     }
