46569b659ce98e581dea53636e0143f63bb1955a
Song Jimin <songwlals050328@gmail.com>
2024-07-26 23:13:48 +0900

    MemberService에 cheangePassword() method 수정

--- src/main/java/hackerton/wakeup/member/service/MemberServiceImpl.java
+++ src/main/java/hackerton/wakeup/member/service/MemberServiceImpl.java
@@ -58,10 +58,10 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public void changePassword(String email,ChangePasswordRequestDTO req) {
-        Member member = memberRepository.findByEmail(req.getEmail()).get();
+        Member member = memberRepository.findByEmail(email).get();
         memberRepository.save(Member.builder()
                 .id(member.getId())
-                .email(member.getEmail())
+                .email(email)
                 .password(passwordEncoder.encode(req.getPassword()))
                 .point(member.getPoint())
                 .build());
