35e09b288b931be3f81483144cedeb8613b814e3
Song Jimin <songwlals050328@gmail.com>
2024-07-27 01:09:26 +0900

    CharacterService 인터페이스의 구현체인 CharacterServiceImpl 생성

---
+++ src/main/java/hackerton/wakeup/character/service/CharacterServiceImpl.java
@@ -0,0 +1,26 @@
+package hackerton.wakeup.character.service;
+
+import hackerton.wakeup.character.entity.Character;
+import hackerton.wakeup.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Primary
+public class CharacterServiceImpl implements CharacterService {
+    private final CharacterService characterService;
+
+    @Override
+    public Character initCharacter(Member member) {
+        return null;
+    }
+
+    @Override
+    public Character getByMemberId(Long member_id) {
+        return null;
+    }
+}
