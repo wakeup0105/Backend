51d78122fb75b768476d29fa39e1bb8433485830
Song Jimin <songwlals050328@gmail.com>
2024-07-28 06:21:44 +0900

    Eyes Entity 생성

---
+++ src/main/java/hackerton/wakeup/eyes/part/entity/Eyes.java
@@ -0,0 +1,13 @@
+package hackerton.wakeup.eyes.part.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Eyes")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Eyes {
+}
