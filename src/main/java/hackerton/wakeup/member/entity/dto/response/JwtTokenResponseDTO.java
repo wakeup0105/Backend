83c9d875b0e05e0dbd055c89314219bfea0e973d
Song Jimin <songwlals050328@gmail.com>
2024-07-27 22:09:05 +0900

    JwtTokenResponseDTO 생성

---
+++ src/main/java/hackerton/wakeup/member/entity/dto/response/JwtTokenResponseDTO.java
@@ -0,0 +1,10 @@
+package hackerton.wakeup.member.entity.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class JwtTokenResponseDTO {
+}
