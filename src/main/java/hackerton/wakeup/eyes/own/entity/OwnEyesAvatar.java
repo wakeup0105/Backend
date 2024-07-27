0479ffee016ca3b410d3fac42fdabe32076b3fc1
Song Jimin <songwlals050328@gmail.com>
2024-07-28 07:01:59 +0900

    OwnEyesAvatar Entity 생성

---
+++ src/main/java/hackerton/wakeup/eyes/own/entity/OwnEyesAvatar.java
@@ -0,0 +1,13 @@
+package hackerton.wakeup.eyes.own.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "OwnEyesTable")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OwnEyesAvatar {
+}
