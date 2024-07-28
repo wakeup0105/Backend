ae7929ec4abac152dee4fc9312dfdaacd481c550
Song Jimin <songwlals050328@gmail.com>
2024-07-29 01:49:20 +0900

    findAllById() method 추가

--- src/main/java/hackerton/wakeup/eyes/own/repository/OwnEyesAvatarRepository.java
+++ src/main/java/hackerton/wakeup/eyes/own/repository/OwnEyesAvatarRepository.java
@@ -4,5 +4,8 @@ import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
 import hackerton.wakeup.eyes.own.entity.OwnEyesAvatarId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OwnEyesAvatarRepository extends JpaRepository<OwnEyesAvatar, OwnEyesAvatarId> {
+    List<OwnEyesAvatar> findAllById(OwnEyesAvatarId id);
 }
