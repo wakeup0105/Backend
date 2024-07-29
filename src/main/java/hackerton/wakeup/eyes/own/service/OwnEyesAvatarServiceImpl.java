7bbc5790e09d9a89a976eb6debcdd7f3418a2239
Song Jimin <songwlals050328@gmail.com>
2024-07-29 19:41:18 +0900

    OwnEyesAvatarService 인터페이스의 구현체인 OwnEyesAvatarServiceImpl 생성

---
+++ src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarServiceImpl.java
@@ -0,0 +1,30 @@
+package hackerton.wakeup.eyes.own.service;
+
+import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
+import hackerton.wakeup.eyes.own.entity.OwnEyesAvatarId;
+import hackerton.wakeup.eyes.own.repository.OwnEyesAvatarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Primary
+public class OwnEyesAvatarServiceImpl implements OwnEyesAvatarService {
+
+    private final OwnEyesAvatarRepository ownEyesAvatarRepository;
+
+    @Override
+    public List<OwnEyesAvatar> getAllOwnEyesAvatars(OwnEyesAvatarId id) {
+        return List.of();
+    }
+
+    @Override
+    public boolean isOwnEyesAvatarExists(Long eyes_id) {
+        return false;
+    }
+}
