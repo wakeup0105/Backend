113266e38e82a208275c07af93f3a1a23488b9da
Song Jimin <songwlals050328@gmail.com>
2024-07-29 19:38:00 +0900

    getAllOwnEyesAvatars() method 추가

--- src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
+++ src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
@@ -1,4 +1,10 @@
 package hackerton.wakeup.eyes.own.service;
 
+import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
+import hackerton.wakeup.eyes.own.entity.OwnEyesAvatarId;
+
+import java.util.List;
+
 public interface OwnEyesAvatarService {
+    List<OwnEyesAvatar> getAllOwnEyesAvatars(OwnEyesAvatarId id);
 }
