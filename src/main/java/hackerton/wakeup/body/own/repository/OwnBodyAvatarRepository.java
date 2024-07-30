3bcca7d83ad97f0932bc1ce81b0a0a74203fb2fd
Song Jimin <songwlals050328@gmail.com>
2024-07-31 00:37:05 +0900

    Own body repository 작성

---
+++ src/main/java/hackerton/wakeup/body/own/repository/OwnBodyAvatarRepository.java
@@ -0,0 +1,11 @@
+package hackerton.wakeup.body.own.repository;
+
+import hackerton.wakeup.body.own.entity.OwnBodyAvatar;
+import hackerton.wakeup.character.entity.CharacterId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OwnBodyAvatarRepository extends JpaRepository<OwnBodyAvatar, Long> {
+    List<OwnBodyAvatar> findAllByCharacterId(CharacterId id);
+}
