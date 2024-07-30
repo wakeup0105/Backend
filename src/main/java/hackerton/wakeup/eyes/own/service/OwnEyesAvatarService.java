f4efe5e081988e0baf496dcede63a668484a06a0
Song Jimin <songwlals050328@gmail.com>
2024-07-31 00:40:09 +0900

    Own body service 작성

--- src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
+++ src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
@@ -1,9 +1,7 @@
 package hackerton.wakeup.eyes.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
-import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
 import hackerton.wakeup.eyes.own.entity.dto.response.AllOwnEyesResponseDTO;
-import hackerton.wakeup.eyes.part.entity.Eyes;
 
 import java.util.List;
 
