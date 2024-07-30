c7d6290ddea5c5db6602bb175fe3645850ebcfc7
Song Jimin <songwlals050328@gmail.com>
2024-07-30 13:23:07 +0900

    mouth repository 작성

--- src/main/java/hackerton/wakeup/eyes/own/repository/OwnEyesAvatarRepository.java
+++ src/main/java/hackerton/wakeup/eyes/own/repository/OwnEyesAvatarRepository.java
@@ -2,7 +2,6 @@ package hackerton.wakeup.eyes.own.repository;
 
 import hackerton.wakeup.character.entity.CharacterId;
 import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
-import hackerton.wakeup.eyes.part.entity.Eyes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
---
+++ src/main/java/hackerton/wakeup/mouth/own/repository/OwnMouthAvatarRepository.java
@@ -0,0 +1,12 @@
+package hackerton.wakeup.mouth.own.repository;
+
+import hackerton.wakeup.character.entity.CharacterId;
+import hackerton.wakeup.mouth.own.entity.OwnMouthAvatar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OwnMouthAvatarRepository extends JpaRepository<OwnMouthAvatar, Long> {
+    List<OwnMouthAvatar> findAllByCharacterId(CharacterId id);
+    boolean existsByMouth_name(String name);
+}
