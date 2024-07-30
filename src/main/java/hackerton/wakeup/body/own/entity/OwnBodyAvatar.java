55cd4e28c5449a27416756a800067aed50ed2314
Song Jimin <songwlals050328@gmail.com>
2024-07-31 00:31:27 +0900

    Own body와 body entity 작성

---
+++ src/main/java/hackerton/wakeup/body/own/entity/OwnBodyAvatar.java
@@ -0,0 +1,36 @@
+package hackerton.wakeup.body.own.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import hackerton.wakeup.body.part.entity.Body;
+import hackerton.wakeup.character.entity.Character;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "OwnBodyAvatar")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OwnBodyAvatar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "body_id")
+    private Body body;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "character", referencedColumnName = "id"),
+            @JoinColumn(name = "member", referencedColumnName = "member_id")
+    })
+    private Character character;
+}
