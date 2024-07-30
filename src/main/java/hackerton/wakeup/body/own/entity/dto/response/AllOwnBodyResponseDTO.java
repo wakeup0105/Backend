0dd822491e51c8de5c988a9eb49fba2b674019b6
Song Jimin <songwlals050328@gmail.com>
2024-07-31 00:35:16 +0900

    Own head dto 작성

---
+++ src/main/java/hackerton/wakeup/body/own/entity/dto/response/AllOwnBodyResponseDTO.java
@@ -0,0 +1,13 @@
+package hackerton.wakeup.body.own.entity.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AllOwnBodyResponseDTO {
+    private Long id;
+    private Long characterId;
+    private String name;
+    private String price;
+}
