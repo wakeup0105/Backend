129c5f42fcbbce0065ea89c5bb457ba13f7726e6
Song Jimin <songwlals050328@gmail.com>
2024-07-30 13:00:51 +0900

    전체조회 로직 반환값 AllOwnEyesResponseDTO로 변경 & 변환 로직 추가

--- src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
+++ src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
@@ -2,11 +2,12 @@ package hackerton.wakeup.eyes.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
 import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
+import hackerton.wakeup.eyes.own.entity.dto.response.AllOwnEyesResponseDTO;
 import hackerton.wakeup.eyes.part.entity.Eyes;
 
 import java.util.List;
 
 public interface OwnEyesAvatarService {
-    List<OwnEyesAvatar> getAllOwnEyesAvatars(CharacterId id);
+    List<AllOwnEyesResponseDTO> getAllOwnEyesAvatars(CharacterId id);
     boolean isOwnEyesAvatarExists(Eyes eye);
 }
--- src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarServiceImpl.java
+++ src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarServiceImpl.java
@@ -2,6 +2,8 @@ package hackerton.wakeup.eyes.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
 import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
+import hackerton.wakeup.eyes.own.entity.dto.OwnEyesDtoConverter;
+import hackerton.wakeup.eyes.own.entity.dto.response.AllOwnEyesResponseDTO;
 import hackerton.wakeup.eyes.own.repository.OwnEyesAvatarRepository;
 import hackerton.wakeup.eyes.part.entity.Eyes;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -20,8 +23,10 @@ public class OwnEyesAvatarServiceImpl implements OwnEyesAvatarService {
     private final OwnEyesAvatarRepository ownEyesAvatarRepository;
 
     @Override
-    public List<OwnEyesAvatar> getAllOwnEyesAvatars(CharacterId id) {
-        return ownEyesAvatarRepository.findAllByCharacterId(id);
+    public List<AllOwnEyesResponseDTO> getAllOwnEyesAvatars(CharacterId id) {
+        return ownEyesAvatarRepository.findAllByCharacterId(id).stream()
+                .map(OwnEyesDtoConverter::allOwnEyesResponseConverter)
+                .collect(Collectors.toList());
     }
 
     @Override
