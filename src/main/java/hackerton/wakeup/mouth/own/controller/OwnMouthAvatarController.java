e4803322612e1abc19cb993a04cc9296807a34b6
Song Jimin <songwlals050328@gmail.com>
2024-07-30 13:30:09 +0900

    mouth controller 작성

---
+++ src/main/java/hackerton/wakeup/mouth/own/controller/OwnMouthAvatarController.java
@@ -0,0 +1,35 @@
+package hackerton.wakeup.mouth.own.controller;
+
+import hackerton.wakeup.character.entity.Character;
+import hackerton.wakeup.character.service.CharacterService;
+import hackerton.wakeup.member.entity.Member;
+import hackerton.wakeup.member.service.MemberService;
+import hackerton.wakeup.mouth.own.entity.dto.response.AllOwnMouthResponseDTO;
+import hackerton.wakeup.mouth.own.service.OwnMouthAvatarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/own/mouth")
+public class OwnMouthAvatarController {
+    private final OwnMouthAvatarService ownMouthAvatarService;
+    private final MemberService memberService;
+    private final CharacterService characterService;
+
+    @GetMapping("/own")
+    @ResponseBody
+    public ResponseEntity<List<AllOwnMouthResponseDTO>> getAllOwnMouthAvatar(Authentication auth) {
+        Member member = memberService.getMemberByEmail(auth.getName()).get();
+        Character character = characterService.getByMemberId(member.getId());
+        List<AllOwnMouthResponseDTO> allOwnMouthsAvatar = ownMouthAvatarService.getAllOwnMouthsAvatar(character.getId());
+        return ResponseEntity.ok(allOwnMouthsAvatar);
+    }
+}
