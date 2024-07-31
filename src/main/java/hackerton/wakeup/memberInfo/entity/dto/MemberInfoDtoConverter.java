737afa054861395717430e18b69cde32b3348eeb
Song Jimin <songwlals050328@gmail.com>
2024-08-01 05:29:54 +0900

    setIntroductionRequestConverter() method 구현

--- src/main/java/hackerton/wakeup/memberInfo/entity/dto/MemberInfoDtoConverter.java
+++ src/main/java/hackerton/wakeup/memberInfo/entity/dto/MemberInfoDtoConverter.java
@@ -1,4 +1,16 @@
 package hackerton.wakeup.memberInfo.entity.dto;
 
+import hackerton.wakeup.memberInfo.entity.MemberInfo;
+import hackerton.wakeup.memberInfo.entity.dto.request.SetIntroductionRequestDTO;
+
 public class MemberInfoDtoConverter {
+
+    public static MemberInfo setIntroductionRequestConverter(MemberInfo memberInfo, SetIntroductionRequestDTO req){
+        return MemberInfo.builder()
+                .id(memberInfo.getId())
+                .nickname(memberInfo.getNickname())
+                .tag(memberInfo.getTag())
+                .introduction(req.getIntroduction())
+                .build();
+    }
 }
