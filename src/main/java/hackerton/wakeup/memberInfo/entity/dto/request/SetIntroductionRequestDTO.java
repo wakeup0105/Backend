d85bffab0bca56c37a2dcea4a9bd4ff86fbea340
Song Jimin <songwlals050328@gmail.com>
2024-08-01 05:24:22 +0900

    introduction 필드 추가

--- src/main/java/hackerton/wakeup/memberInfo/entity/dto/request/SetIntroductionRequestDTO.java
+++ src/main/java/hackerton/wakeup/memberInfo/entity/dto/request/SetIntroductionRequestDTO.java
@@ -1,9 +1,12 @@
 package hackerton.wakeup.memberInfo.entity.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SetIntroductionRequestDTO {
+    @NotBlank(message = "자기소개는 최소 한 글자 이상 입력해야합니다.")
+    private String introduction;
 }
