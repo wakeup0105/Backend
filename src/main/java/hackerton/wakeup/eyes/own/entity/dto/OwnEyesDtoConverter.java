57e088f87df7ee9c05f4570c8f9ac87487f63dc9
Song Jimin <songwlals050328@gmail.com>
2024-08-02 16:11:57 +0900

    converter인 buyEyesResponseConverter() method 작성

--- src/main/java/hackerton/wakeup/eyes/own/entity/dto/OwnEyesDtoConverter.java
+++ src/main/java/hackerton/wakeup/eyes/own/entity/dto/OwnEyesDtoConverter.java
@@ -2,6 +2,7 @@ package hackerton.wakeup.eyes.own.entity.dto;
 
 import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
 import hackerton.wakeup.eyes.own.entity.dto.response.AllOwnEyesResponseDTO;
+import hackerton.wakeup.eyes.own.entity.dto.response.BuyEyesResponseDTO;
 
 public class OwnEyesDtoConverter {
 
@@ -12,5 +13,14 @@ public class OwnEyesDtoConverter {
         dto.setName(res.getEyes().getName());
         dto.setPrice(res.getEyes().getPrice());
         return dto;
+    }
+
+    public static BuyEyesResponseDTO buyEyesResponseConverter(OwnEyesAvatar res){
+        BuyEyesResponseDTO dto = new BuyEyesResponseDTO();
+        dto.setNickname(res.getCharacter().getMember().getMemberInfo().getNickname() + "#"
+        + res.getCharacter().getMember().getMemberInfo().getTag());
+        dto.setAvatarName(res.getEyes().getName());
+        dto.setPrice(res.getEyes().getPrice());
+        dto.setPoint(res.getCharacter().getMember().getPoint());
     }
 }
