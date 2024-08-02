80e5e179b72869db31877db84acc907c4f90095a
Song Jimin <songwlals050328@gmail.com>
2024-08-02 16:41:56 +0900

    head service에 구매 로직 구현

--- src/main/java/hackerton/wakeup/head/own/service/OwnHeadAvatarService.java
+++ src/main/java/hackerton/wakeup/head/own/service/OwnHeadAvatarService.java
@@ -2,9 +2,12 @@ package hackerton.wakeup.head.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
 import hackerton.wakeup.head.own.entity.dto.response.AllOwnHeadResponseDTO;
+import hackerton.wakeup.head.own.entity.dto.response.BuyHeadResponseDTO;
+import hackerton.wakeup.member.entity.Member;
 
 import java.util.List;
 
 public interface OwnHeadAvatarService {
     List<AllOwnHeadResponseDTO> getAllOwnHeadAvatar(CharacterId id);
+    BuyHeadResponseDTO buyHead(Member member, String name);
 }
--- src/main/java/hackerton/wakeup/head/own/service/OwnHeadAvatarServiceImpl.java
+++ src/main/java/hackerton/wakeup/head/own/service/OwnHeadAvatarServiceImpl.java
@@ -1,9 +1,15 @@
 package hackerton.wakeup.head.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
+import hackerton.wakeup.head.own.entity.OwnHeadAvatar;
 import hackerton.wakeup.head.own.entity.dto.OwnHeadDtoConverter;
 import hackerton.wakeup.head.own.entity.dto.response.AllOwnHeadResponseDTO;
+import hackerton.wakeup.head.own.entity.dto.response.BuyHeadResponseDTO;
 import hackerton.wakeup.head.own.repository.OwnHeadAvatarRepository;
+import hackerton.wakeup.head.part.entity.Head;
+import hackerton.wakeup.head.part.repository.HeadRepository;
+import hackerton.wakeup.member.entity.Member;
+import hackerton.wakeup.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -17,6 +23,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Primary
 public class OwnHeadAvatarServiceImpl implements OwnHeadAvatarService {
+    private final MemberRepository memberRepository;
+    private final HeadRepository headRepository;
     private final OwnHeadAvatarRepository ownHeadAvatarRepository;
 
     @Override
@@ -24,5 +32,22 @@ public class OwnHeadAvatarServiceImpl implements OwnHeadAvatarService {
         return ownHeadAvatarRepository.findAllByCharacterId(id).stream()
                 .map(OwnHeadDtoConverter::allOwnHeadResponseConverter)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public BuyHeadResponseDTO buyHead(Member member, String name) {
+        Head findAvatar = headRepository.findByName(name);
+        if (findAvatar == null || findAvatar.getPrice() > member.getPoint()) {
+            return null;
+        }
+        memberRepository.save(Member.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .point(member.getPoint() - findAvatar.getPrice())
+                .memberInfo(member.getMemberInfo())
+                .character(member.getCharacter()).build());
+        OwnHeadAvatar save = ownHeadAvatarRepository.save(OwnHeadDtoConverter.saveOwnHeadAvatar(member, findAvatar));
+        return OwnHeadDtoConverter.buyHeadResponseConverter(save);
     }
 }
