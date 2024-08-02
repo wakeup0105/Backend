958c1ef51ec96b9a4133f122bd367e8b177d2781
Song Jimin <songwlals050328@gmail.com>
2024-08-02 15:57:02 +0900

    OwnEyesAvatar Service에 buyEye method 추가

--- src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
+++ src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarService.java
@@ -1,10 +1,13 @@
 package hackerton.wakeup.eyes.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
+import hackerton.wakeup.eyes.own.entity.OwnEyesAvatar;
 import hackerton.wakeup.eyes.own.entity.dto.response.AllOwnEyesResponseDTO;
+import hackerton.wakeup.member.entity.Member;
 
 import java.util.List;
 
 public interface OwnEyesAvatarService {
     List<AllOwnEyesResponseDTO> getAllOwnEyesAvatars(CharacterId id);
+    OwnEyesAvatar buyEyes(Member member, String name);
 }
--- src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarServiceImpl.java
+++ src/main/java/hackerton/wakeup/eyes/own/service/OwnEyesAvatarServiceImpl.java
@@ -6,6 +6,8 @@ import hackerton.wakeup.eyes.own.entity.dto.OwnEyesDtoConverter;
 import hackerton.wakeup.eyes.own.entity.dto.response.AllOwnEyesResponseDTO;
 import hackerton.wakeup.eyes.own.repository.OwnEyesAvatarRepository;
 import hackerton.wakeup.eyes.part.entity.Eyes;
+import hackerton.wakeup.eyes.part.repository.EyesRepository;
+import hackerton.wakeup.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -21,11 +23,17 @@ import java.util.stream.Collectors;
 public class OwnEyesAvatarServiceImpl implements OwnEyesAvatarService {
 
     private final OwnEyesAvatarRepository ownEyesAvatarRepository;
+    private final EyesRepository eyesRepository;
 
     @Override
     public List<AllOwnEyesResponseDTO> getAllOwnEyesAvatars(CharacterId id) {
         return ownEyesAvatarRepository.findAllByCharacterId(id).stream()
                 .map(OwnEyesDtoConverter::allOwnEyesResponseConverter)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public OwnEyesAvatar buyEyes(Member member, String name) {
+        return null;
     }
 }
