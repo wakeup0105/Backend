c507ee8b79cbe44a41a15d615db37982515f4c02
Song Jimin <songwlals050328@gmail.com>
2024-08-02 16:31:28 +0900

    mouth에 dto와 converter 추가

--- src/main/java/hackerton/wakeup/mouth/own/entity/dto/OwnMouthDtoConverter.java
+++ src/main/java/hackerton/wakeup/mouth/own/entity/dto/OwnMouthDtoConverter.java
@@ -1,7 +1,10 @@
 package hackerton.wakeup.mouth.own.entity.dto;
 
+import hackerton.wakeup.eyes.part.entity.Mouth;
+import hackerton.wakeup.member.entity.Member;
 import hackerton.wakeup.mouth.own.entity.OwnMouthAvatar;
 import hackerton.wakeup.mouth.own.entity.dto.response.AllOwnMouthResponseDTO;
+import hackerton.wakeup.mouth.own.entity.dto.response.BuyMouthResponseDTO;
 
 public class OwnMouthDtoConverter {
 
@@ -12,5 +15,22 @@ public class OwnMouthDtoConverter {
         dto.setName(res.getMouth().getName());
         dto.setPrice(res.getMouth().getPrice());
         return dto;
+    }
+
+    public static BuyMouthResponseDTO buyMouthResponseConverter(OwnMouthAvatar res){
+        BuyMouthResponseDTO dto = new BuyMouthResponseDTO();
+        dto.setNickname(res.getCharacter().getMember().getMemberInfo().getNickname() + "#"
+                + res.getCharacter().getMember().getMemberInfo().getTag());
+        dto.setAvatarName(res.getMouth().getName());
+        dto.setPrice(res.getMouth().getPrice());
+        dto.setPoint(res.getCharacter().getMember().getPoint());
+
+        return dto;
+    }
+
+    public static OwnMouthAvatar saveOwnMouthAvatar(Member member, Mouth mouth){
+        return OwnMouthAvatar.builder()
+                .mouth(mouth)
+                .character(member.getCharacter()).build();
     }
 }
---
+++ src/main/java/hackerton/wakeup/mouth/own/entity/dto/response/BuyMouthResponseDTO.java
@@ -0,0 +1,13 @@
+package hackerton.wakeup.mouth.own.entity.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BuyMouthResponseDTO {
+    private String nickname;
+    private String avatarName;
+    private int price;
+    private int point;
+}
