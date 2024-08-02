3fe4df3ff9e705a8f4e1b38634bc6e53e239ff97
Song Jimin <songwlals050328@gmail.com>
2024-08-02 20:06:47 +0900

    body service에 구매 로직 구현

--- src/main/java/hackerton/wakeup/body/own/service/OwnBodyAvatarService.java
+++ src/main/java/hackerton/wakeup/body/own/service/OwnBodyAvatarService.java
@@ -1,10 +1,13 @@
 package hackerton.wakeup.body.own.service;
 
 import hackerton.wakeup.body.own.entity.dto.response.AllOwnBodyResponseDTO;
+import hackerton.wakeup.body.own.entity.dto.response.BuyBodyResponseDTO;
 import hackerton.wakeup.character.entity.CharacterId;
+import hackerton.wakeup.member.entity.Member;
 
 import java.util.List;
 
 public interface OwnBodyAvatarService {
     List<AllOwnBodyResponseDTO> getAllOwnBodyAvatars(CharacterId id);
+    BuyBodyResponseDTO buyBody(Member member, String name);
 }
--- src/main/java/hackerton/wakeup/body/own/service/OwnBodyAvatarServiceImpl.java
+++ src/main/java/hackerton/wakeup/body/own/service/OwnBodyAvatarServiceImpl.java
@@ -1,9 +1,15 @@
 package hackerton.wakeup.body.own.service;
 
+import hackerton.wakeup.body.own.entity.OwnBodyAvatar;
 import hackerton.wakeup.body.own.entity.dto.OwnBodyDtoConverter;
 import hackerton.wakeup.body.own.entity.dto.response.AllOwnBodyResponseDTO;
+import hackerton.wakeup.body.own.entity.dto.response.BuyBodyResponseDTO;
 import hackerton.wakeup.body.own.repository.OwnBodyAvatarRepository;
+import hackerton.wakeup.body.part.entity.Body;
+import hackerton.wakeup.body.part.repository.BodyRepository;
 import hackerton.wakeup.character.entity.CharacterId;
+import hackerton.wakeup.member.entity.Member;
+import hackerton.wakeup.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -17,6 +23,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Primary
 public class OwnBodyAvatarServiceImpl implements OwnBodyAvatarService {
+    private final MemberRepository memberRepository;
+    private final BodyRepository bodyRepository;
     private final OwnBodyAvatarRepository ownBodyAvatarRepository;
 
     @Override
@@ -24,5 +32,22 @@ public class OwnBodyAvatarServiceImpl implements OwnBodyAvatarService {
         return ownBodyAvatarRepository.findAllByCharacterId(id).stream()
                 .map(OwnBodyDtoConverter::allOwnBodyResponseConverter)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public BuyBodyResponseDTO buyBody(Member member, String name) {
+        Body findAvatar = bodyRepository.findByName(name);
+        if (findAvatar == null || findAvatar.getPrice() > member.getPoint()) {
+            return null;
+        }
+        memberRepository.save(Member.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .point(member.getPoint() - findAvatar.getPrice())
+                .memberInfo(member.getMemberInfo())
+                .character(member.getCharacter()).build());
+        OwnBodyAvatar save = ownBodyAvatarRepository.save(OwnBodyDtoConverter.saveOwnBodyAvatar(member, findAvatar));
+        return OwnBodyDtoConverter.buyBodyResponseConverter(save);
     }
 }
