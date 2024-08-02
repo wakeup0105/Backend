50b25833f430cc1674211a917d94ba423afcad3a
Song Jimin <songwlals050328@gmail.com>
2024-08-03 01:53:11 +0900

    OwnMouth 장착 converter 작성

--- src/main/java/hackerton/wakeup/mouth/own/entity/dto/OwnMouthDtoConverter.java
+++ src/main/java/hackerton/wakeup/mouth/own/entity/dto/OwnMouthDtoConverter.java
@@ -1,5 +1,6 @@
 package hackerton.wakeup.mouth.own.entity.dto;
 
+import hackerton.wakeup.character.entity.Character;
 import hackerton.wakeup.eyes.part.entity.Mouth;
 import hackerton.wakeup.member.entity.Member;
 import hackerton.wakeup.mouth.own.entity.OwnMouthAvatar;
@@ -26,6 +27,17 @@ public class OwnMouthDtoConverter {
         dto.setPoint(res.getCharacter().getMember().getPoint());
 
         return dto;
+    }
+
+    public static Character equipMouthConverter(Character character, String name){
+        return Character.builder()
+                .id(character.getId())
+                .member(character.getMember())
+                .level(character.getLevel())
+                .exp(character.getExp())
+                .eyes(character.getEyes()).mouth(name).head(character.getHead()).body(character.getBody())
+                .ownEyesAvatars(character.getOwnEyesAvatars()).ownMouthAvatars(character.getOwnMouthAvatars())
+                .ownHeadAvatars(character.getOwnHeadAvatars()).ownBodyAvatars(character.getOwnBodyAvatars()).build();
     }
 
     public static OwnMouthAvatar saveOwnMouthAvatar(Member member, Mouth mouth){
