1dab7ab22225039b729d980d1a5aac622b762e45
Song Jimin <songwlals050328@gmail.com>
2024-08-02 16:35:17 +0900

    mouth service에 구매 로직 구현

--- src/main/java/hackerton/wakeup/mouth/own/service/OwnMouthAvatarService.java
+++ src/main/java/hackerton/wakeup/mouth/own/service/OwnMouthAvatarService.java
@@ -1,10 +1,13 @@
 package hackerton.wakeup.mouth.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
+import hackerton.wakeup.member.entity.Member;
 import hackerton.wakeup.mouth.own.entity.dto.response.AllOwnMouthResponseDTO;
+import hackerton.wakeup.mouth.own.entity.dto.response.BuyMouthResponseDTO;
 
 import java.util.List;
 
 public interface OwnMouthAvatarService {
     List<AllOwnMouthResponseDTO> getAllOwnMouthsAvatar(CharacterId id);
+    BuyMouthResponseDTO buyMouth(Member member, String name);
 }
--- src/main/java/hackerton/wakeup/mouth/own/service/OwnMouthAvatarServiceImpl.java
+++ src/main/java/hackerton/wakeup/mouth/own/service/OwnMouthAvatarServiceImpl.java
@@ -1,9 +1,15 @@
 package hackerton.wakeup.mouth.own.service;
 
 import hackerton.wakeup.character.entity.CharacterId;
+import hackerton.wakeup.eyes.part.entity.Mouth;
+import hackerton.wakeup.member.entity.Member;
+import hackerton.wakeup.member.repository.MemberRepository;
+import hackerton.wakeup.mouth.own.entity.OwnMouthAvatar;
 import hackerton.wakeup.mouth.own.entity.dto.OwnMouthDtoConverter;
 import hackerton.wakeup.mouth.own.entity.dto.response.AllOwnMouthResponseDTO;
+import hackerton.wakeup.mouth.own.entity.dto.response.BuyMouthResponseDTO;
 import hackerton.wakeup.mouth.own.repository.OwnMouthAvatarRepository;
+import hackerton.wakeup.mouth.part.repository.MouthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -17,6 +23,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Primary
 public class OwnMouthAvatarServiceImpl implements OwnMouthAvatarService {
+    private final MemberRepository memberRepository;
+    private final MouthRepository mouthRepository;
     private final OwnMouthAvatarRepository ownMouthAvatarRepository;
 
     @Override
@@ -24,5 +32,22 @@ public class OwnMouthAvatarServiceImpl implements OwnMouthAvatarService {
         return ownMouthAvatarRepository.findAllByCharacterId(id).stream()
                 .map(OwnMouthDtoConverter::allOwnMouthResponseConverter)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public BuyMouthResponseDTO buyMouth(Member member, String name) {
+        Mouth findAvatar = mouthRepository.findByName(name);
+        if (findAvatar == null || findAvatar.getPrice() > member.getPoint()) {
+            return null;
+        }
+        memberRepository.save(Member.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .point(member.getPoint() - findAvatar.getPrice())
+                .memberInfo(member.getMemberInfo())
+                .character(member.getCharacter()).build());
+        OwnMouthAvatar save = ownMouthAvatarRepository.save(OwnMouthDtoConverter.saveOwnMouthAvatar(member, findAvatar));
+        return OwnMouthDtoConverter.buyMouthResponseConverter(save);
     }
 }
