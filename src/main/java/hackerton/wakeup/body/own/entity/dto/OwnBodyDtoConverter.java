0447935eb7c0b341a9a53c01c03aeac95bd7bbc2
Song Jimin <songwlals050328@gmail.com>
2024-08-03 01:55:56 +0900

    OwnBody 장착 converter 작성

--- src/main/java/hackerton/wakeup/body/own/entity/dto/OwnBodyDtoConverter.java
+++ src/main/java/hackerton/wakeup/body/own/entity/dto/OwnBodyDtoConverter.java
@@ -4,6 +4,7 @@ import hackerton.wakeup.body.own.entity.OwnBodyAvatar;
 import hackerton.wakeup.body.own.entity.dto.response.AllOwnBodyResponseDTO;
 import hackerton.wakeup.body.own.entity.dto.response.BuyBodyResponseDTO;
 import hackerton.wakeup.body.part.entity.Body;
+import hackerton.wakeup.character.entity.Character;
 import hackerton.wakeup.member.entity.Member;
 
 public class OwnBodyDtoConverter {
@@ -25,6 +26,17 @@ public class OwnBodyDtoConverter {
         dto.setPrice(res.getBody().getPrice());
         dto.setPoint(res.getCharacter().getMember().getPoint());
         return dto;
+    }
+
+    public static Character equipBodyConverter(Character character, String name){
+        return Character.builder()
+                .id(character.getId())
+                .member(character.getMember())
+                .level(character.getLevel())
+                .exp(character.getExp())
+                .eyes(character.getEyes()).mouth(character.getMouth()).head(character.getHead()).body(name)
+                .ownEyesAvatars(character.getOwnEyesAvatars()).ownMouthAvatars(character.getOwnMouthAvatars())
+                .ownHeadAvatars(character.getOwnHeadAvatars()).ownBodyAvatars(character.getOwnBodyAvatars()).build();
     }
 
     public static OwnBodyAvatar saveOwnBodyAvatar(Member member, Body body){
