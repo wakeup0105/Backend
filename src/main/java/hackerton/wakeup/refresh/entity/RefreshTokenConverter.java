c4552250e500e9a3c5dc09c73b6d15ef41b7c6f9
Song Jimin <songwlals050328@gmail.com>
2024-08-05 06:24:42 +0900

    createTokenConverter 작성

--- src/main/java/hackerton/wakeup/refresh/entity/RefreshTokenConverter.java
+++ src/main/java/hackerton/wakeup/refresh/entity/RefreshTokenConverter.java
@@ -1,4 +1,15 @@
 package hackerton.wakeup.refresh.entity;
 
+import hackerton.wakeup.member.entity.Member;
+
+import java.time.Instant;
+
 public class RefreshTokenConverter {
+
+    public static RefreshToken createTokenConverter(String token, Member member, Instant expires) {
+        return RefreshToken.builder()
+                .token(token)
+                .member(member)
+                .expiryTime(expires).build();
+    }
 }
