fe4fa8eaadbf5b173b5b220314b13e35158be235
Song Jimin <songwlals050328@gmail.com>
2024-08-05 06:20:59 +0900

    expiryTime 필드 추가

--- src/main/java/hackerton/wakeup/refresh/entity/RefreshToken.java
+++ src/main/java/hackerton/wakeup/refresh/entity/RefreshToken.java
@@ -2,10 +2,13 @@ package hackerton.wakeup.refresh.entity;
 
 import hackerton.wakeup.member.entity.Member;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "RefreshToken")
@@ -24,4 +27,7 @@ public class RefreshToken {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @NotNull
+    private Instant expiryTime;
 }
