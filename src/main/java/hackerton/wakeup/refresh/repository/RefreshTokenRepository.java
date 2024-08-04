22089eb7fcf5fd692be8adeadad19a89abc9f5e4
Song Jimin <songwlals050328@gmail.com>
2024-08-05 07:00:58 +0900

    findByToken() method 추가

--- src/main/java/hackerton/wakeup/refresh/repository/RefreshTokenRepository.java
+++ src/main/java/hackerton/wakeup/refresh/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package hackerton.wakeup.refresh.repository;
 import hackerton.wakeup.refresh.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
 }
