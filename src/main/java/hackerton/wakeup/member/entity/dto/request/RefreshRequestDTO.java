23083c9801867e1083ff10e949d109a979e0d855
Song Jimin <songwlals050328@gmail.com>
2024-08-05 10:50:17 +0900

    refreshToken 필드 추가

--- src/main/java/hackerton/wakeup/member/entity/dto/request/RefreshRequestDTO.java
+++ src/main/java/hackerton/wakeup/member/entity/dto/request/RefreshRequestDTO.java
@@ -1,4 +1,8 @@
 package hackerton.wakeup.member.entity.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class RefreshRequestDTO {
+    @NotBlank
+    private String refreshToken;
 }
