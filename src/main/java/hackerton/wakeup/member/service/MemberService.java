128f30eea52f126b79c695d91cee6f01b2911f7f
Song Jimin <songwlals050328@gmail.com>
2024-08-05 10:14:42 +0900

    Member Service에 createRefreshToken() method 추가

--- src/main/java/hackerton/wakeup/member/service/MemberService.java
+++ src/main/java/hackerton/wakeup/member/service/MemberService.java
@@ -4,6 +4,7 @@ import hackerton.wakeup.member.entity.Member;
 import hackerton.wakeup.member.entity.dto.request.ChangePasswordRequestDTO;
 import hackerton.wakeup.member.entity.dto.request.JoinRequestDTO;
 import hackerton.wakeup.member.entity.dto.request.LoginRequestDTO;
+import hackerton.wakeup.refresh.entity.RefreshToken;
 
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ public interface MemberService {
     boolean checkEmailDuplication(String email);
     void joinMember(JoinRequestDTO req);
     Member loginMember(LoginRequestDTO req);
+    RefreshToken createRefreshToken(String email, Long expiresInSeconds);
     void changePassword(String email, ChangePasswordRequestDTO req);
     void sendVerificationEmail(String email);
     Optional<Member> getMemberById(Long id);
