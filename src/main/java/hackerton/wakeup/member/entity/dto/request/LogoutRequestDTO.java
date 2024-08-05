0f5fbe6223e66ceffd5bdaee6ba037e33343703f
Song Jimin <songwlals050328@gmail.com>
2024-08-06 05:10:58 +0900

    email 필드 추가

--- src/main/java/hackerton/wakeup/member/entity/dto/request/LogoutRequestDTO.java
+++ src/main/java/hackerton/wakeup/member/entity/dto/request/LogoutRequestDTO.java
@@ -1,9 +1,12 @@
 package hackerton.wakeup.member.entity.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LogoutRequestDTO {
+    @NotBlank
+    private String email;
 }
